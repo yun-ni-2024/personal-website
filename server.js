7d6ca1671d8199ff88e2b0c3191812e9003e55ca
yunni <scanocdii@gmail.com>
2024-11-02 22:39:44 +0800

    listen to all ports

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
